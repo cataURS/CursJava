5e7ae9629686e4942e99ba085094c7addf312a56
Catalin Ursu (Engineering) <catalin.ursu@softelligence.net>
2022-06-15 23:25:59 +0300

    curs 6 push

---
+++ src/curs6/ContinueBreakExample.java
@@ -0,0 +1,23 @@
+package curs6;
+
+public class ContinueBreakExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		for(int i=0;i<10;i++) {
+			
+			if(i == 5) {
+				
+				//break;
+				continue;
+				
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ src/curs6/PasswordValidator.java
@@ -0,0 +1,89 @@
+package curs6;
+
+import java.util.Scanner;
+
+public class PasswordValidator {
+
+	/*
+	 * Facem un program care valideaza o parola pe baza unor reguli :
+	 * Reguli:
+	 * 1.parola trebuie sa fie de minim 10 caractere
+	 * 2.parola trebuie sa contina upper case
+	 * 3.parola nu trebuie sa fie la fel ca username
+	 * 
+	 * intrebam utilizatorul un username si o parola
+	 * il informam care sunt regulile parolei
+	 * 
+	 * Daca parola este valida atunci printam "Parola valida!"
+	 * Daca nu, il informam care reguli nu au fost respectate
+	 * 
+	 * 
+	 */
+	String username;
+	Scanner scan  =  new Scanner(System.in);
+	boolean valid = true;
+	
+	
+	public void getUsername() {
+		System.out.println("Introdu username : ");
+		username = scan.nextLine();  
+	}
+	
+	public String getPassword() {
+		System.out.println("Introdu o parola :");
+		String pass = scan.nextLine();
+		return pass;
+		//return scan.nextLine()
+	}
+	
+	public void printPasswordRules() {
+		System.out.println("Reguli :");
+		System.out.println("parola trebuie sa fie de minim 10 caractere");
+		System.out.println("parola trebuie sa contina upper case");
+		System.out.println("parola nu trebuie sa fie la fel ca username");
+
+	}
+	
+	public void checkPasswordRules(String pass) {
+		//valid =  true;
+		
+		if(pass.length() <10) {
+			valid = false;
+			System.out.println("parola trebuie sa fie de minim 10 caractere");
+			
+		}
+		if(pass.equals(pass.toLowerCase())) {
+			valid = false;
+			System.out.println("parola trebuie sa contina upper case");
+
+		}
+		if(pass.equals(username)) {
+			valid = false;
+			System.out.println("parola nu trebuie sa fie la fel ca username");
+
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		PasswordValidator validator = new PasswordValidator();
+		
+		validator.printPasswordRules();
+		validator.getUsername();
+		
+		do {
+		
+			validator.checkPasswordRules(validator.getPassword());
+	//		if(validator.valid == true) {
+	//			break;
+	//		}
+			
+		}while(!validator.valid);
+		
+		System.out.println("Parola valida!");
+		
+	}
+	
+}
